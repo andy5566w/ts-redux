ec0b6e6aa55ac587652b4d2e20c285e516cd6d2f
andy tsai <andy5566w@gmail.com>
2021-04-01 22:21:21 +0800

    doc: 上傳

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actionCreators } from '../state'
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('')
+  const dispatch = useDispatch()
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    dispatch(actionCreators.searchRepositories(term))
+  }
+
   return (
     <div>
-      <form action="">
-        <input type="text" />
+      <form action="" onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
         <button>search</button>
       </form>
     </div>

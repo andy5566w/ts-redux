245df5426b279e7561dec29b023ae01a95a01997
andy tsai <andy5566w@gmail.com>
2021-04-02 11:25:31 +0800

    feat: 建立rootState type

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import { useActions } from '../hooks/useActions'
+import { useSelector } from 'react-redux'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const { searchRepositories } = useActions()
+  const { data, error, message } = useSelector(
+    (state: any) => state.repositories
+  )
+  console.table({ data, error, message })
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 

6472cdd0da4432954de3589af6fd3df351372472
andy tsai <andy5566w@gmail.com>
2021-04-02 11:30:12 +0800

    feat: 幫useSelector加上type

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import { useActions } from '../hooks/useActions'
-import { useSelector } from 'react-redux'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const { searchRepositories } = useActions()
-  const { data, error, message } = useSelector(
-    (state: any) => state.repositories
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
   )
-  console.table({ data, error, message })
+  console.table({ data, error, loading })
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
